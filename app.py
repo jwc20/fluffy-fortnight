c690dfbb387b791be602f56b4526d36fdf315460
cjw <choi.jae100@gmail.com>
2024-11-23 11:55:02 +0900

    use sqlite3

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, FormField, FieldList, IntegerField, Form
 from wtforms.validators import Optional
 from collections import namedtuple
+import sqlite3
 
 def main():
     print("Hello from fluffy-fortnight!")
