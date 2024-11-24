a4f5e0d48c08deba849c1b1f52d33c0f4a3281a7
jwc20 <choij2015@gmail.com>
2024-11-24 23:57:28 +0900

    Update render problems

--- ff/problems/problems.py
+++ ff/problems/problems.py
@@ -7,16 +7,31 @@ from ff.problems import bp
 def index():
     db = get_db()
     problems = db.execute(
-        'SELECT * FROM problems'
+        'SELECT p.id, p.leetcode_number, p.title, p.link, p.description, p.difficulty_id as difficulty, p.solution_notes, p.time_complexity, p.space_complexity, p.is_premium, p.created_at, p.updated_at, p.deleted_at, p.deleted, GROUP_CONCAT(pa.name) as patterns '
+        'FROM problems p '
+        'JOIN problem_patterns pp ON p.id=pp.problem_id '
+        'JOIN patterns pa ON pp.pattern_id=pa.id '
+        'GROUP BY p.id '
+        'ORDER BY p.leetcode_number;'
     ).fetchall()
-    return render_template('problems/index.html', problems=problems)
+
+    patterns = {}
+    for problem in problems:
+        problem_patterns = problem['patterns'].split(',')
+        for pattern in problem_patterns:
+            if pattern in patterns:
+                patterns[pattern] += 1
+            else:
+                patterns[pattern] = 1
+
+    return render_template('problems/index.html', problems=problems, patterns=patterns)
 
 
 @bp.route('/neetcode150')
 def neetcode150():
     db = get_db()
     problems = db.execute(
-        'SELECT p.id, p.leetcode_number, p.title, p.link, p.description, p.difficulty_id, p.solution_notes, p.time_complexity, p.space_complexity, p.is_premium, p.created_at, p.updated_at, p.deleted_at, p.deleted, t.name as tag_name, GROUP_CONCAT(pa.name) as patterns '
+        'SELECT p.id, p.leetcode_number, p.title, p.link, p.description, p.difficulty_id as difficulty, p.solution_notes, p.time_complexity, p.space_complexity, p.is_premium, p.created_at, p.updated_at, p.deleted_at, p.deleted, t.name as tag_name, GROUP_CONCAT(pa.name) as patterns '
         'FROM problems p '
         'JOIN problem_tags pt ON p.id=pt.problem_id '
         'JOIN tags t ON pt.tag_id=t.id '
@@ -43,7 +58,7 @@ def neetcode150():
 def blind75():
     db = get_db()
     problems = db.execute(
-        'SELECT p.id, p.leetcode_number, p.title, p.link, p.description, p.difficulty_id, p.solution_notes, p.time_complexity, p.space_complexity, p.is_premium, p.created_at, p.updated_at, p.deleted_at, p.deleted, t.name as tag_name, GROUP_CONCAT(pa.name) as patterns '
+        'SELECT p.id, p.leetcode_number, p.title, p.link, p.description, p.difficulty_id as difficulty, p.solution_notes, p.time_complexity, p.space_complexity, p.is_premium, p.created_at, p.updated_at, p.deleted_at, p.deleted, t.name as tag_name, GROUP_CONCAT(pa.name) as patterns '
         'FROM problems p '
         'JOIN problem_tags pt ON p.id=pt.problem_id '
         'JOIN tags t ON pt.tag_id=t.id '
