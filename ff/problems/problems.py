c8d52433849e2759cf088c942342c83d3a8a967b
jwc20 <choij2015@gmail.com>
2024-11-25 02:50:37 +0900

    Format

--- ff/problems/problems.py
+++ ff/problems/problems.py
@@ -4,28 +4,27 @@ from ff.db import get_db
 from ff.problems import bp
 
 
-
 def write_query(endpoint):
     if endpoint is not None:
         return (
             'SELECT p.id, p.leetcode_number, p.title, p.link, p.description, p.difficulty_id as difficulty, p.solution_notes, p.time_complexity, p.space_complexity, p.is_premium, p.created_at, p.updated_at, p.deleted_at, p.deleted, t.name as tag_name, GROUP_CONCAT(pa.name, ", ") as patterns '
-            'FROM problems p '
-            'LEFT JOIN problem_tags pt ON p.id=pt.problem_id '
-            'LEFT JOIN tags t ON pt.tag_id=t.id '
-            'LEFT JOIN problem_patterns pp ON p.id=pp.problem_id '
-            'LEFT JOIN patterns pa ON pp.pattern_id=pa.id '
+            "FROM problems p "
+            "LEFT JOIN problem_tags pt ON p.id=pt.problem_id "
+            "LEFT JOIN tags t ON pt.tag_id=t.id "
+            "LEFT JOIN problem_patterns pp ON p.id=pp.problem_id "
+            "LEFT JOIN patterns pa ON pp.pattern_id=pa.id "
             'WHERE t.endpoint="%s" '
-            'GROUP BY p.id '
-            'ORDER BY p.leetcode_number;' % endpoint
+            "GROUP BY p.id "
+            "ORDER BY p.leetcode_number;" % endpoint
         )
     else:
         return (
             'SELECT p.id, p.leetcode_number, p.title, p.link, p.description, p.difficulty_id as difficulty, p.solution_notes, p.time_complexity, p.space_complexity, p.is_premium, p.created_at, p.updated_at, p.deleted_at, p.deleted, GROUP_CONCAT(pa.name, ", ") as patterns '
-            'FROM problems p '
-            'LEFT JOIN problem_patterns pp ON p.id=pp.problem_id '
-            'LEFT JOIN patterns pa ON pp.pattern_id=pa.id '
-            'GROUP BY p.id '
-            'ORDER BY p.leetcode_number;'
+            "FROM problems p "
+            "LEFT JOIN problem_patterns pp ON p.id=pp.problem_id "
+            "LEFT JOIN patterns pa ON pp.pattern_id=pa.id "
+            "GROUP BY p.id "
+            "ORDER BY p.leetcode_number;"
         )
 
 
@@ -33,13 +32,14 @@ def execute_query(query):
     db = get_db()
     return db.execute(query).fetchall()
 
+
 def get_patterns(problems):
     patterns = {}
     for problem in problems:
-        if problem['patterns'] is None:
+        if problem["patterns"] is None:
             continue
         else:
-            problem_patterns = problem['patterns'].split(',')
+            problem_patterns = problem["patterns"].split(",")
             for pattern in problem_patterns:
                 if pattern in patterns:
                     patterns[pattern] += 1
@@ -48,25 +48,29 @@ def get_patterns(problems):
     return patterns
 
 
-@bp.route('/')
+@bp.route("/")
 def index():
     query = write_query(None)
     problems = execute_query(query)
     patterns = get_patterns(problems)
-    return render_template('problems/index.html', problems=problems, patterns=patterns)
+    return render_template("problems/index.html", problems=problems, patterns=patterns)
 
 
-@bp.route('/neetcode150')
+@bp.route("/neetcode150")
 def neetcode150():
-    query = write_query('neetcode150')
+    query = write_query("neetcode150")
     problems = execute_query(query)
     patterns = get_patterns(problems)
-    return render_template('problems/neetcode150.html', problems=problems, patterns=patterns)
+    return render_template(
+        "problems/neetcode150.html", problems=problems, patterns=patterns
+    )
 
-@bp.route('/blind75')
+
+@bp.route("/blind75")
 def blind75():
-    query = write_query('blind75')
+    query = write_query("blind75")
     problems = execute_query(query)
     patterns = get_patterns(problems)
-    return render_template('problems/blind75.html', problems=problems, patterns=patterns)
-
+    return render_template(
+        "problems/blind75.html", problems=problems, patterns=patterns
+    )
