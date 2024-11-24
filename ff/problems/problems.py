88d02d5d0e1911cceb669291bb64c94e43ee6b6c
jwc20 <choij2015@gmail.com>
2024-11-24 17:03:26 +0900

    Edit to fetch and render problems with 'neetcode' tags

--- ff/problems/problems.py
+++ ff/problems/problems.py
@@ -10,3 +10,17 @@ def index():
         'SELECT * FROM problems'
     ).fetchall()
     return render_template('problems/index.html', problems=problems)
+
+
+@bp.route('/neetcode')
+def neetcode():
+    db = get_db()
+    problems = db.execute(
+        'SELECT *, pa.name as pattern FROM problems p '
+        'JOIN problem_tags pt ON p.id = pt.problem_id '
+        'JOIN tags t ON pt.tag_id = t.id '
+        'JOIN problem_patterns pp ON p.id = pp.problem_id '
+        'JOIN patterns pa ON pp.pattern_id = pa.id '
+        'WHERE t.name = "neetcode"'
+    ).fetchall()
+    return render_template('problems/neetcode.html', problems=problems)
