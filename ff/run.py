e997c9c0ef5b1b1fa58b35b800b3c8c8f367602c
jwc20 <choij2015@gmail.com>
2024-12-14 15:18:18 +0900

    Edit to run this script for debugging in pycharm

--- ff/run.py
+++ ff/run.py
@@ -1,14 +1,22 @@
 """
-For debugging
+For debugging in pycharm
 """
+# from ff import create_app
+#
+# app = create_app()
+#
+# def main():
+#     print("welcome to ff!")
+#
+# if __name__ == "__main__":
+#     main()
+#     port = app.config.get('PORT', 5000)
+#     app.run(host='localhost', port=port, debug=True)
+
+from flask.cli import FlaskGroup
 from ff import create_app
 
-app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
-def main():
-    print("welcome to ff!")
-
-if __name__ == "__main__":
-    main()
-    port = app.config.get('PORT', 5000)
-    app.run(host='localhost', port=port, debug=True)
+if __name__ == '__main__':
+    cli()
